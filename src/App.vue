1dfebb8038fb88c322448b2a89b1b8610469bc55
Igor Bezmestin <igor-rock@list.ru>
2019-05-02 20:01:45 +0300

    Настройка проекта: подключил внешние scss и normalize.css

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  h1.beach Hello world!
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "/styles/main.scss"
+</style>
