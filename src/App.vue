236168041fa77c55ad527b54de8457b632eff06c
Igor Bezmestin <igor-rock@list.ru>
2019-05-05 22:21:11 +0300

    Сверстал третью страницу

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template lang="pug">  
   .app-root
-    .keypad
-      button(type="button").keypad__btn
-    nav.menu
-      ul.menu__list
-        li.menu__item
-          a(href="#").menu__link.active Contacts
-        li.menu__item
-          a(href="#").menu__link History    
+
+    //- .keypad
+    //-   button(type="button").keypad__btn
+
+    //- nav.menu
+    //-   ul.menu__list
+    //-     li.menu__item
+    //-       a(href="#").menu__link.active Contacts
+    //-     li.menu__item
+    //-       a(href="#").menu__link History 
+
     //- section.contacts
     //-   .container
     //-     .search
@@ -42,39 +45,86 @@
     //-               .contacts__number 
     //-             .contacts__information
 
-    section.history
-      .container   
-        .contacts__table
-          ul.contacts__list
-            li(
-              v-for="contact in 10"
-              :key="contact"
-            ).contacts__item
-              .contacts__row
-                .contacts__hamburger
-                  .contacts__hamburger-icon
-                    .contacts__hamburger-line(
-                      v-for="line in 3"
-                      :key="line"
-                    )
-                .contacts__description
-                  .contacts__name Giacomo Auilizzoni
-                  .contacts__number +18121234567
-                .contacts__date
-                  .contacts__day Today
-                  .contacts__year at 12:10PM
-            li(
-              v-for="dummy in 100"
-              :key="dummy"
-            ).contacts__item.contacts__item--dummy
-              .contacts__row
-                .contacts__avatar
-                  //- img(alt="Avatar" src="#").contacts__image
-                .contacts__description
-                  .contacts__name  
-                  .contacts__number 
-                .contacts__information
+    //- section.history
+    //-   .container   
+    //-     .contacts__table
+    //-       ul.contacts__list
+    //-         li(
+    //-           v-for="contact in 10"
+    //-           :key="contact"
+    //-         ).contacts__item
+    //-           .contacts__row
+    //-             .contacts__hamburger
+    //-               .contacts__hamburger-icon
+    //-                 .contacts__hamburger-line(
+    //-                   v-for="line in 3"
+    //-                   :key="line"
+    //-                 )
+    //-             .contacts__description
+    //-               .contacts__name Giacomo Auilizzoni
+    //-               .contacts__number +18121234567
+    //-             .contacts__date
+    //-               .contacts__day Today
+    //-               .contacts__year at 12:10PM
+    //-         li(
+    //-           v-for="dummy in 100"
+    //-           :key="dummy"
+    //-         ).contacts__item.contacts__item--dummy
+    //-           .contacts__row
+    //-             .contacts__avatar
+    //-             .contacts__description
+    //-               .contacts__name  
+    //-               .contacts__number 
+    //-             .contacts__information
               
+    section.contactdata
+      .container
+
+        form.form            
+          .form__wrapper    
+            .form__controll        
+              label.form__element.form__element--button.back
+                button(type="button").form__button
+              label.form__element.form__element--button.save
+                button(type="button").form__button
+            label.form__element
+              .form__avatar
+                img(alt="Аватар" src="./images/avatars/contact.jpg").form__avatar-img
+            label.form__element
+              input(type="text" placeholder="First name").form__input
+            label.form__element
+              input(type="text" placeholder="Last name").form__input
+            label.form__element
+              input(type="text" placeholder="Phone number").form__input
+            label.form__element
+              input(type="text" placeholder="Date of birth mm/dd/yyyy").form__input
+            .chart
+              .chart__box
+                svg(viewBox="0 0 345 200" preserveAspectRatio="xMidYMid meet").chart__item
+                  g
+                    polygon(
+                      points="10 200, 10 50, 60 50, 60 200"
+                      stroke="black"
+                      stroke-width="1"
+                      fill="firebrick"
+                    )
+                    polygon(
+                      points="60 200, 60 30, 110 30, 110 200"
+                      stroke="black"
+                      stroke-width="1"
+                      fill="blue"
+                    )
+                    line(x1="0" y1="200" x2="0" y2="0" stroke="black" stroke-width="3%")
+                    line(x1="0" y1="200" x2="345" y2="200" stroke="black" stroke-width="3%")
+                    
+                    //- rect(x="10" y="200" width="30" height="50")
+
+
+        .keypad.keypad--phone
+          button(type="button").keypad__btn
+      
+    
+
 
 </template>
 
