40a83d93f3f1cc489c1c86c422c01492c562912a
Igor Bezmestin <igor-rock@list.ru>
2019-05-06 22:52:11 +0300

    Сверстал секцию с numpad

--- src/App.vue
+++ src/App.vue
@@ -29,21 +29,13 @@
     //-         ).contacts__item
     //-           .contacts__row
     //-             .contacts__avatar
-    //-               img(alt="Avatar" src="./images/avatars/contact.jpg").contacts__image
+    //-               .contacts__avatar-box
+    //-                 img(alt="Avatar" src="./images/avatars/contact.jpg").contacts__image
     //-             .contacts__description
     //-               .contacts__name Giacomo Auilizzoni
-    //-               .contacts__number +18121234567
+    //-               .contacts__number +78121234567
     //-             .contacts__information
-    //-         li(
-    //-           v-for="dummy in 100"
-    //-           :key="dummy"
-    //-         ).contacts__item.contacts__item--dummy
-    //-           .contacts__row
-    //-             .contacts__avatar                  
-    //-             .contacts__description
-    //-               .contacts__name  
-    //-               .contacts__number 
-    //-             .contacts__information
+    //-               .contacts__information-box            
 
     //- section.history
     //-   .container   
@@ -62,69 +54,117 @@
     //-                 )
     //-             .contacts__description
     //-               .contacts__name Giacomo Auilizzoni
-    //-               .contacts__number +18121234567
+    //-               .contacts__number +78121234567
     //-             .contacts__date
     //-               .contacts__day Today
-    //-               .contacts__year at 12:10PM
-    //-         li(
-    //-           v-for="dummy in 100"
-    //-           :key="dummy"
-    //-         ).contacts__item.contacts__item--dummy
-    //-           .contacts__row
-    //-             .contacts__avatar
-    //-             .contacts__description
-    //-               .contacts__name  
-    //-               .contacts__number 
-    //-             .contacts__information
+    //-               .contacts__year at 12:10PM            
               
-    section.contactdata
+    //- section.contactdata
+    //-   .container
+    //-     form.form            
+    //-       .form__wrapper    
+    //-         .form__controll        
+    //-           label.form__element.form__element--button.back
+    //-             button(type="button").form__button
+    //-           label.form__element.form__element--button.save
+    //-             button(type="button").form__button
+    //-         label.form__element
+    //-           .form__avatar
+    //-             img(alt="Аватар" src="./images/avatars/contact.jpg").form__avatar-img
+    //-         label.form__element
+    //-           input(type="text" placeholder="First name").form__input
+    //-         label.form__element
+    //-           input(type="text" placeholder="Last name").form__input
+    //-         label.form__element
+    //-           input(type="text" placeholder="Phone number").form__input
+    //-         label.form__element
+    //-           input(type="text" placeholder="Date of birth mm/dd/yyyy").form__input
+    //-         .chart
+    //-           .chart__box
+    //-             svg(viewBox="0 0 345 200" preserveAspectRatio="xMidYMid meet").chart__item
+    //-               g
+    //-                 polygon(
+    //-                   points="10 200, 10 50, 60 50, 60 200"
+    //-                   stroke="black"
+    //-                   stroke-width="1"
+    //-                   fill="firebrick"
+    //-                 )
+    //-                 polygon(
+    //-                   points="60 200, 60 30, 110 30, 110 200"
+    //-                   stroke="black"
+    //-                   stroke-width="1"
+    //-                   fill="blue"
+    //-                 )
+    //-                 line(x1="0" y1="200" x2="0" y2="0" stroke="black" stroke-width="3%")
+    //-                 line(x1="0" y1="200" x2="345" y2="200" stroke="black" stroke-width="3%")
+    //-     .keypad.keypad--phone
+    //-       button(type="button").keypad__btn
+      
+    section.phone
       .container
-
-        form.form            
+        form.form.form--phone            
           .form__wrapper    
-            .form__controll        
+            .form__controll.phone
               label.form__element.form__element--button.back
                 button(type="button").form__button
               label.form__element.form__element--button.save
                 button(type="button").form__button
-            label.form__element
-              .form__avatar
-                img(alt="Аватар" src="./images/avatars/contact.jpg").form__avatar-img
-            label.form__element
-              input(type="text" placeholder="First name").form__input
-            label.form__element
-              input(type="text" placeholder="Last name").form__input
-            label.form__element
-              input(type="text" placeholder="Phone number").form__input
-            label.form__element
-              input(type="text" placeholder="Date of birth mm/dd/yyyy").form__input
-            .chart
-              .chart__box
-                svg(viewBox="0 0 345 200" preserveAspectRatio="xMidYMid meet").chart__item
-                  g
-                    polygon(
-                      points="10 200, 10 50, 60 50, 60 200"
-                      stroke="black"
-                      stroke-width="1"
-                      fill="firebrick"
-                    )
-                    polygon(
-                      points="60 200, 60 30, 110 30, 110 200"
-                      stroke="black"
-                      stroke-width="1"
-                      fill="blue"
-                    )
-                    line(x1="0" y1="200" x2="0" y2="0" stroke="black" stroke-width="3%")
-                    line(x1="0" y1="200" x2="345" y2="200" stroke="black" stroke-width="3%")
-                    
-                    //- rect(x="10" y="200" width="30" height="50")
 
 
-        .keypad.keypad--phone
-          button(type="button").keypad__btn
-      
-    
+            .form__match-table
+              .contacts__table
+                ul.contacts__list
+                  li(
+                    v-for="contact in 11"
+                    :key="contact"
+                  ).contacts__item
+                    .contacts__row
+                      .contacts__avatar
+                        .contacts__avatar-box
+                          img(alt="Avatar" src="./images/avatars/contact.jpg").contacts__image
+                      .contacts__description
+                        .contacts__name Giacomo Auilizzoni
+                        .contacts__number +78121234567
+                      .contacts__information
+                        .contacts__information-box
+            .form__row
+              label.form__element
+                input(type="text" placeholder="Please type a number...").form__input.phonenumber
+              label.form__element.form__element--button.backspace
+                button(type="button").form__button
 
+            .horizontal-line
+              svg(viewBox="0 0 1000 100" preserveAspectRatio="none").horizontal-line__icon
+                ellipse(
+                  cx="500" cy="50" rx="500" ry="8"
+                  fill="#414c63"
+                )      
+            
+            .numpad
+              ul.numpad__list
+                li(
+                  v-for="num in 9"
+                  :key="num"
+                ).numpad__item
+                  label.numpad__container
+                    .numpad__text 5
+                    button(typ.numpad__buttone="button")
+                li.numpad__item
+                  label.numpad__container
+                    .numpad__text *
+                    button(typ.numpad__buttone="button")
+                li.numpad__item
+                  label.numpad__container.zero
+                    .numpad__text 0
+                    .numpad__text +
+                    button(typ.numpad__buttone="button")
+                li.numpad__item
+                  label.numpad__container
+                    .numpad__text #
+                    button(typ.numpad__buttone="button")
+
+
+       
 
 </template>
 
