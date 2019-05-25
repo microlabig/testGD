26a2d4e2a2e653c312b9510055fc2df98b4293b8
Igor Bezmestin <igor-rock@list.ru>
2019-05-25 21:38:42 +0300

    Сделал график звонков

--- src/components/parts/chart.vue
+++ src/components/parts/chart.vue
@@ -4,29 +4,47 @@
             svg(
                 :viewBox="viewBoxCoordinates" 
                 preserveAspectRatio="none"
-            ).chart__item
+            ).chart__svg
                 g(
-                    v-for="object in sortByDateOutgoing"
-                    :key="object.dateTime"
+                    v-for="column in columns"
+                    :key="column.index"
                 )
                     polygon(
-                        points="10 200, 10 50, 60 50, 60 200"
-                        stroke="black"
-                        fill="firebrick"
-                    )                    
+                        :points="column.polygonCoordinates"
+                        stroke='#414c63'
+                        :stroke-width="strokeWidth/2"
+                        :fill="column.color"
+                    )
+                    g(
+                        
+                    )
+                        text(
+                            :x="column.quantityCoordinatesX" :y="column.quantityCoordinatesY"
+                            :font-size="fontSize"
+                            :fill="column.color"
+                        ) {{column.quantityText}}
+                    text(
+                        :x="column.dateCoordinatesX" :y="column.dateCoordinatesY"
+                        :font-size="fontSize"
+                        :fill="column.color"     
+                        :transform="column.dateTransformRotate"                       
+                    ) {{column.dateText}}
+                        
                 line(
-                    x1="0" 
-                    :y1="ViewBoxX2Y2.y2" 
-                    x2="0" 
-                    y2="0" 
-                    stroke="black"
+                    :x1="linesCoordinates[0].x1" 
+                    :y1="linesCoordinates[0].y1" 
+                    :x2="linesCoordinates[0].x2" 
+                    :y2="linesCoordinates[0].y2" 
+                    stroke='#414c63'
+                    :stroke-width="strokeWidth"
                 )
                 line(
-                    x1="0" 
-                    :y1="ViewBoxX2Y2.y2" 
-                    :x2="ViewBoxX2Y2.x2" 
-                    :y2="ViewBoxX2Y2.y2" 
-                    stroke="black"
+                    :x1="linesCoordinates[1].x1" 
+                    :y1="linesCoordinates[1].y1" 
+                    :x2="linesCoordinates[1].x2" 
+                    :y2="linesCoordinates[1].y2" 
+                    stroke='#414c63'
+                    :stroke-width="strokeWidth/2"
                 )
 </template>
 
@@ -41,74 +59,123 @@ export default {
     data() {
         return {
             sortByDateOutgoing: [],
-            maxX: 0,
-            maxY: 0,
-            offsetYFromLoft: 200,
-            offsetXFromRightBorder: 200,
-            ViewBoxX2Y2: {
-                x2: 0,
-                y2: 0
-            },
-            viewBoxCoordinates: "",
-            columns: [],
+
+            maxValue: 100, // максимальное значение вьюбокса (100 по-умалчанию)
+            fontSize: "5px",
+
+            strokeWidth: 1,
+
+            offsetYFromLoft: 22,
+
+            widthColumn: 3,
+            
+            columns: [], // массив параметров для отображения колонок в svg-элементе
+        }
+    },
+
+    computed: {
+        // заполним настройки viewBox svg-графика
+        viewBoxCoordinates() {
+            return "0 " + "0 " + this.maxValue + " " + this.maxValue;
+        },
+
+        // заполним координаты координатных линий графика 
+        linesCoordinates() {
+            return [
+                {
+                    x1: 0,
+                    y1: this.maxValue - this.offsetYFromLoft, // для отображения даты
+                    x2: 0,
+                    y2: 0
+                }, // Оу
+                {
+                    x1: 0,
+                    y1: this.maxValue - this.offsetYFromLoft,
+                    x2: this.maxValue,
+                    y2: this.maxValue - this.offsetYFromLoft,                
+                } // Ox
+            ]
         }
     },
 
     created() {
-
-        //console.log(this.outgoingCalls);
+        let isOdd = false; // для смены цвета колонки
+        
+        // сделаем копию массива звонков текущему пользователю
         this.sortByDateOutgoing = [...this.outgoingCalls];
 
-        //2
+        // отсортируем по дате (сначала старые)
         sortArrayByCallAllDateTime(this.sortByDateOutgoing);
 
-        //3
-        const offset = 150, 
-              widthColumn = 30,
-              heightColumn = 0;                   
+        // найдем максимальное значение по Оу (максимальное значение количества звонков из массива звонков)
+        for (let i = 0; i < this.sortByDateOutgoing.length; i++) 
+            if (parseInt(this.sortByDateOutgoing[i].quantity) > this.maxValue) this.maxValue = parseInt(this.sortByDateOutgoing[i].quantity);
+        
+        if (this.maxValue < 100) this.maxValue = 100; // 20 - максимальное число звонков (чтобы отображался красиво)
+        //console.log(this.maxValue);
 
-        //4
-        for (let i = 0; i < this.sortByDateOutgoing.length; i++) {
-            if (parseInt(this.sortByDateOutgoing[i].quantity) > this.maxY) this.maxY = parseInt(this.sortByDateOutgoing[i].quantity);
+        // вычислим расстояние до вершины графика 
+        this.offsetYFromLoft = this.maxValue * 22 / 100; // 22% - место для отображения даты звонков
+        this.strokeWidth = this.maxValue * 1 / 100; // 1% 
+        this.fontSize = this.maxValue * 5 / 100 + "px"; // 5%
+
+        // вычислим ширину колонки = 4% от максимального значения        
+        this.widthColumn = this.maxValue * 3 / 100;
+                
+        // вычислим все Х0 (центральные точки) для каждой отображаемой колонки (для каждого элемента массива outgoing)
+        const pointer = this.maxValue / (this.sortByDateOutgoing.length + 1); // граничная точка по Ох  каждой колонки
+        let pointersArray = [], i = 1, x0 = 0; // массив центральных точек по Ох для каждой колонки, индекс, центральня точка по Ох
+        while (1) {
+            x0 = Math.round(pointer * i);
+            if (x0 >= this.maxValue) 
+                break;
+            else 
+                pointersArray.push(x0);
+            i++;
         }
-        console.log('maxY=', this.maxY);        
-
-        //5
-        this.maxX = this.sortByDateOutgoing.length * (offset + widthColumn) * 2;
-        console.log('maxX=',this.maxX);
-
-        //6
-        this.ViewBoxX2Y2.x2 = this.maxX + this.offsetXFromRightBorder;
-        this.ViewBoxX2Y2.y2 = this.maxY + this.offsetYFromLoft;
-        //console.log(ViewBoxX2Y2);
-        this.viewBoxCoordinates = "0 " + "0 " + this.ViewBoxX2Y2.x2 + " " + this.ViewBoxX2Y2.y2;
-        //console.log(this.viewBoxCoordinates);
         
+        // заполним дополнительные параметры отображения элементов на графике
+        for (let i = 0; i < pointersArray.length; i++) {
+            const item = this.sortByDateOutgoing[i], // теккущее значение массива outgoing
+                date = new Date(item.dateTime), // дата 
+                transformFS = this.fontSize.match(/^\d*[.,]?\d*/),
+                transform = this.widthColumn * 50 / 100; // для трансформации даты
+            let obj = {}, // объект с координатами точек (х1 y1, x2 y2, x3 y3, x4 y4)
+                // преобразуем дату для графика
+                m = date.getMonth() + 1, d = date.getDate(), y = date.getFullYear() % 100, // месяц, день, год    
+                // сформируем двузначные значения
+                monthStr = (m < 10) ? ("0" + m) : ("" + m),
+                dayStr = (d < 10) ? ("0" + d) : ("" + d),
+                yearStr = (y < 10) ? ("0" + y) : ("" + y),
+                quantity = item.quantity >= 100 ? 93 : item.quantity; // для координат полигона (93 - это 93% для отображения кол-ва звонков)             
 
-        //7
-        
-        //8
-        for (let i = 0; i < this.sortByDateOutgoing.length; i++) {
-            const item = this.sortByDateOutgoing[i],
-                  date = new Date(item.dateTime);
+            obj = {   
+                x1: Math.round(pointersArray[i] - this.widthColumn),
+                y1: this.linesCoordinates[0].y1,
+                x2: Math.round(pointersArray[i] + this.widthColumn),
+                y2: Math.round(this.linesCoordinates[1].y2 - this.linesCoordinates[1].y2 * quantity / 100) // 6 - место для отображения даты
+            };
 
-            let obj = {};
+            isOdd = !isOdd;
 
-            //TODO: сделать координаты
-
-            obj = {
-                points: "",
-                dateText: date.toLocaleDateString()
-            }            
-
-            console.log(obj.dateText);
-            
-            //this.column
+            // в массив параметров
+            this.columns.push({  
+                index: i, // индекс элемента в массиве (для :key d элементе Vue при использовании v-for)
+                color: isOdd ? 'firebrick' : 'dodgerblue',
+                polygonCoordinates: "" + obj.x1 + " " + obj.y1 + "," + " " +
+                                         obj.x1 + " " + obj.y2 + "," + " " +
+                                         obj.x2 + " " + obj.y2 + "," + " " +
+                                         obj.x2 + " " + obj.y1, // сформированная строка с координатами полигона
+                quantityCoordinatesX: "" + (pointersArray[i] - (("" + item.quantity).length*0.5) * transformFS / 2), 
+                quantityCoordinatesY: "" + (obj.y2 - transform), 
+                quantityText: "" + item.quantity, // строка с количеством звонков
+                dateCoordinatesX: "" + (pointersArray[i] - this.widthColumn), 
+                dateCoordinatesY: "" + (obj.y1),
+                //dateTransformRotate: "translate(-" + transform +", " + transform +") rotate(40 " + (pointersArray[i]) + " " + Math.round(obj.y1) + ")",
+                dateTransformRotate: "translate(-" + transform + ", " + (transform * 3) +") rotate(90 " + (pointersArray[i]) + " " + Math.round(obj.y1) + ")",
+                dateText: "" + dayStr + "." + monthStr + "." + yearStr // строка с датой звонков
+            });
         }
-
-
-
-        
     }
 }
 </script>
